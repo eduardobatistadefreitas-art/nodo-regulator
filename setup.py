0139f01d37bc12f5782993e9a34ad05b5026cc59
eduardobatistadefreitas-art <eduardobatistadefreitas@gmail.com>
2026-06-25 19:23:21 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nodo-regulator",
+    version="0.1.0",
+    author="Engenharia de Dados",
+    description="Filtro espectral de alta performance para redução de dimensionalidade O(N)",
+    py_modules=["nodo_regulator"],  # Mapeia diretamente o arquivo único
+    install_requires=[
+        "numpy>=1.20.0",
+        "scipy>=1.7.0",
+        "scikit-learn>=1.0.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
